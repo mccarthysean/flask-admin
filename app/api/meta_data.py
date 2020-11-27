d4a6d8bf611233a552dc0a346b292400e4c4a755
Sean McCarthy <sean.mccarthy@hotmail.com>
2020-11-27 13:39:48 -0700

    updated post request to updated record's color if cell/element already exists

--- app/api/meta_data.py
+++ app/api/meta_data.py
@@ -32,22 +32,28 @@ class MetaDataList(Resource):
 
     @meta_data_namespace.expect(meta_data_model, validate=True)
     def post(self):
-        """Creates a single record"""
+        """Creates a single record. If record exists, it updates it."""
         post_data = request.get_json()
         id_cell = post_data.get('id_cell')
         element = post_data.get('element')
         color = post_data.get('color')
         response_object = {}
 
-        record = MetaData.query.filter_by(id_cell=id_cell).first()
-        if record:
-            response_object['message'] = 'That record already exists.'
-            return response_object, 400
+        record = MetaData.query.filter_by(id_cell=id_cell, element=element).first()
+        if record is None:
+            # Create a new record
+            record = MetaData(id_cell=id_cell, element=element, color=color)
+            db.session.add(record)
+            response_object['message'] = 'Record was created!'
+        else:
+            # Update existing record, like a put request
+            # response_object['message'] = 'That record already exists.'
+            # return response_object, 400
+            record.color = color
+            response_object['message'] = 'Record was updated!'
 
-        db.session.add(MetaData(id_cell=id_cell, element=element, color=color))
         db.session.commit()
 
-        response_object['message'] = f'{id_cell} was added!'
         return response_object, 201
 
 
